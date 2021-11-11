9ec4e014f79d226bf57e01768407f481518a40e5
Lucas2003-18 <70024244+Lucas2003-18@users.noreply.github.com>
2021-11-11 11:29:47 -0300

    Create cliente.php

---
+++ model/cliente.php
@@ -0,0 +1,17 @@
+<?php
+
+class Cliente{
+
+    //atributos
+    private $codigo;
+    private $nome;
+    private $endereco;
+    private $telefone;
+
+    //getters and setters
+    
+   
+
+}
+
+?>
