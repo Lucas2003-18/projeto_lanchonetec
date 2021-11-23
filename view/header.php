dd822ad27a4a72ee850070b87b8a559222ecf97f
Lucas2003-18 <70024244+Lucas2003-18@users.noreply.github.com>
2021-11-23 15:21:27 -0300

    Mudanças do framework
    Mudei o bootstrap 5.0 para a 4.6.1 por ser mais estável e ter mais coisas explicadas

--- view/header.php
+++ view/header.php
@@ -8,7 +8,6 @@
 
     <!--Bootstrap and CSS-->
     <link rel="stylesheet" href="public/css/main.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-    <title>Página Inicial</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">    <title>Página Inicial</title>
 </head>
 <body>
--- view/index.php
+++ view/index.php
@@ -8,32 +8,149 @@ $navbar = require_once './view/shared/navbar.php';
 
  
 
+<div class="row">
+    <div class="col-md-8" style="background-color:blue;">
         <div class="row">
-            <div class="col-md-8" style="background-color:blue;">
-                <div class="row">dados</div>
-                <div class="row">
-                    <div class="col-md-8" style="background-color:blueviolet;">tipos de lanches</div>
-                    <div class="col" style="background-color: aqua;">adicionais</div>
-                </div>
-                <div class="row">
-                    <div class="col-md-8" style="background-color:blueviolet;">
-                        <div class="row">
-                            <div class="col" style="background-color:blue;">bebida</div>
-                            <div class="col" style="background-color: red;">bebida gelada</div>
-                        </div>
-                        <div class="row" style="background-color: brown;">tipos de bebidas</div>
-                        <div class="row" style="background-color: blue;">Data do pedido</div>  
-                    </div>
-                    <div class="col" style="background-color: purple;">adicionais</div>
-                </div>
-                <div class="row" style="background-color: blueviolet;">Observaçoes</div>
+          <div class="mb-3 row">
+           <h1>Dados</h1>
+           <h6>Código do pedido</h6>
+            <div class="col-sm-10">
+              <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="email@example.com">
             </div>
-
-
-
-            <div class="col" style="background-color: red;">pedido</div>
+          </div>
+        </div>
+        <h6>Nome do Cliente</h6>
+        <div class="form-group">
+          <label for="exampleFormControlSelect1">Example select</label>
+          <select class="form-control" id="exampleFormControlSelect1">
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+          </select>
+          <button type="button" class="btn btn-outline-secondary">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
+              <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+              <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+            </svg>
+          </button>
+        </div>
+        <div class="row">
+            <div class="col-md-8" style="background-color:blueviolet;">
+              <h6>Tipos de lanches</h6>
+              <div class="form-group">
+                <label for="exampleFormControlSelect1">Example select</label>
+                <select class="form-control" id="exampleFormControlSelect1">
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                  <option>5</option>
+                </select>
+                <button type="button" class="btn btn-outline-secondary">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
+                    <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+                    <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+                  </svg>
+                </button>
+              </div>
+            </div>
+            <div class="col" style="background-color: aqua;">
+              <h6>Adicionais</h6>
+              <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
+                <label class="form-check-label" for="inlineRadio1">Sim</label>
+              </div>
+              <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
+                <label class="form-check-label" for="inlineRadio2">Não</label>
+              </div>
+            </div>
+            <div class="col" style="background-color: purple;">
+              <h6>Adicionais</h6>
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                <label class="form-check-label" for="flexCheckDefault">
+                  Pure
+                </label>
+              </div>
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
+                <label class="form-check-label" for="flexCheckChecked">
+                  Sua mãe
+                </label>
+              </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-8" style="background-color:blueviolet;">
+                <div class="row">
+                    <div class="col" style="background-color:blue;">
+                      <h6>Bebida?</h6>
+                      <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
+                        <label class="form-check-label" for="inlineRadio1">Sim</label>
+                      </div>
+                      <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
+                        <label class="form-check-label" for="inlineRadio2">Não</label>
+                      </div>
+                    </div>
+                    <div class="col" style="background-color: red;">
+                      <h6>Bebida Gelada?</h6>
+                      <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
+                        <label class="form-check-label" for="inlineRadio1">Sim</label>
+                      </div>
+                      <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
+                        <label class="form-check-label" for="inlineRadio2">Não</label>
+                      </div>
+                    </div>
+                </div>
+                <div class="row" style="background-color: brown;">
+                  <h6>Tipos de bebidas</h6>
+                  <div class="form-group">
+                    <label for="exampleFormControlSelect1">Example select</label>
+                    <select class="form-control" id="exampleFormControlSelect1">
+                      <option>1</option>
+                      <option>2</option>
+                      <option>3</option>
+                      <option>4</option>
+                      <option>5</option>
+                    </select>
+                    <button type="button" class="btn btn-outline-secondary">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
+                        <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+                        <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+                      </svg>
+                    </button>
+                  </div>
+                <div class="row" style="background-color: blue;">Data do pedido</div>  
+            </div>
+            
+            </div>
+        </div>
+        <div class="row" style="background-color: blueviolet;">
+          <h6>Observaçoes</h6>
+          <div class="form-floating">
+            <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea"></textarea>
+            <label for="floatingTextarea">Pedido</label>
+          </div>
         </div>
     </div>
+
+
+
+    <div class="col" style="background-color: red;">
+      <div class="form-floating">
+        <textarea style="resize: none" class="form-control" placeholder="Leave a comment here" id="floatingTextarea" disabled></textarea>
+        <label for="floatingTextarea">Pedido</label>
+      </div>
+    </div>
+</div>
+</div>
       
 <?php //footer da página
 $url_atual = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
