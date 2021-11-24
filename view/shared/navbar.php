01087832c71684a34a2487708577f7cbf107bb6d
Lucas2003-18 <70024244+Lucas2003-18@users.noreply.github.com>
2021-11-24 10:18:36 -0300

    a

--- view/shared/navbar.php
+++ view/shared/navbar.php
@@ -1,8 +1,8 @@
-<div class="container" style="width: 100000px; height: 100vh;">
+
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container-fluid">
               <a class="navbar-brand" href="#">
-                  <img src="../img/img_exemplo.jpg" alt="" width="30" height="24" class="d-inline-block align-text-top">
+                  <img src="../public/img/lanchotec.jpg" alt="" width="30" height="24" class="d-inline-block align-text-top">
                   LanchoTec
                 </a>
               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
@@ -43,76 +43,4 @@
               </div>
             </div>
           </nav>
-            <?php
-            if(isset($_SESSION['logged']) != true){
-                ?>
-                <form action="?action=signin" method="post">
-                    <button class="btn btn-outline-success mx-1" type="submit">Sign In</button>
-                </form>
-                <form action="?action=signup" method="post">
-                    <button class="btn btn-outline-primary mx-1" type="submit">Sign Up</button>
-                </form>
-                <?php
-            }
-            if(isset($_SESSION['logged']) == true){
-                ?>
-                <ul class="navbar-nav ml-auto mb-2 mb-lg-0 d-flex">
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Menu
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a href="?page=profile" class="dropdown-item">Profile</a>
-                            <a href="?action=quit" class="dropdown-item">Log out</a>    
-                        </div>
-                    </li>
-                </ul>
-                
-                <?php
-            }
-            ?>
-        </div>
-    </div>
-</nav>
-<?php
-    if(isset($_SESSION['logged']) == true){
-?>
-<nav class="navbar-expand-lg navbar-primary bg-primary text-white">
-    <div class="container">
-        <div class="d-flex wrap justify-content-around">
-                <ul class="navbar-nav d-flex">
-                    <li class="nav-item">
-                        <a href="?action=create_board" class="nav-link text-white">Create a Board</a>
-                    </li>
-                </ul>
-                <ul class="navbar-nav d-flex">
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle text-white" href="#" id="toolbarDropdownT" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Teams
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="toolbarDropdownT">
-                            <a href="?action=create_team" class="dropdown-item">Create</a>
-                            <div class="dropdown-divider"></div>
-                            <div class="dropdown-item disabled">Your teams</div>
-                            <div class="dropdown-divider"></div>
-                            <?php
-                                $teams = call_user_func(array('TeamController','listTeamsUser'),$_SESSION['id']);
-                                if(isset($teams) && !empty($teams)){
-                                    foreach($teams as $team){
-                            ?>
-                            <a href="?page=team&team_id=<?php echo $team->getId(); ?>" class="dropdown-item"><?php echo $team->getName(); ?></a>   
-                            <?php
-                                    }
-                                }
-                            ?> 
-                        </div>
-                    </li>
-                </ul>
-        </div>
-    </div>
-</nav>
-<?php
-}
-    
-
-?>
+            
